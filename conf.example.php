232c65998e0b1d16f489310744e371079800c418
Alexandre Willerval <alexandre@willerval.net>
2018-09-23 13:19:39 +0000

    Add conf.exemple.php

---
+++ conf.example.php
@@ -0,0 +1,13 @@
+<?php
+  $DB_CONF = array(
+    "SERVER" => "localhost",
+    "USER" => "root",
+    "PASSWORD" => "xxxxxx"
+  );
+  $FRAMEWORKS = array(
+    "BOOSTED_PATH" => "vendor/orange-opensource/orange-boosted-bootstrap/dist/",
+    "JQUERY_PATH" => "vendor/components/jquery/",
+    "JQUERY_UI_PATH" => "vendor/components/jqueryui/",
+    "FONT_AWESOME_PATH" => "vendor/components/font-awesome/css/",
+  );
+?>
